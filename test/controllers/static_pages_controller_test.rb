151bd116975a9890d1450b30681579f805252c9f
kento <kozukata1993@gmail.com>
2019-10-19 18:19:10 +0900

    edit static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,21 @@
-require 'test_helper'
+require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | Umigame App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Umigame App"
   end
 
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Umigame App"
+  end
 end
