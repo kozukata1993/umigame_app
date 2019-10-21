10e6cd7d52831f198a38e1e94326ed526956c6c3
kento <kozukata1993@gmail.com>
2019-10-21 17:24:27 +0900

    edit application_helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,13 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | Umigame App"
+    assert_select "title", "Umigame App"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Umigame App"
+    assert_select "title", "Umigame App"
   end
 
   test "should get about" do
