c7b44d8ef87ff4c84e6d20b5432f90456cf5c4cc
kento <kozukata1993@gmail.com>
2019-10-24 22:03:27 +0900

    deviseでログイン機能追加

--- Gemfile
+++ Gemfile
@@ -13,6 +13,10 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'haml-rails'
 gem 'erb2haml'
+gem 'devise'
+gem 'omniauth-facebook'
+gem 'omniauth-twitter'
+gem 'omniauth-google-oauth2'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,23 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get "login", to: "devise/sessions#new", as: :new_user_session
+    post "login", to: "devise/sessions#create", as: :user_session
+    delete "logout", to: "devise/sessions#destroy", as: :destroy_user_session
+    get "signup", to: "devise_/registrations#new", as: :new_user_registration
+    post "signup", to: "devise_/registrations#create", as: :user_registration
+    get "signup/cancel", to: "devise_/registrations#cancel", as: :cancel_user_registration
+    get "user", to: "devise_/registrations#edit", as: :edit_user_registration
+    patch "user", to: "devise_/registrations#update", as: nil
+    put "user", to: "devise_/registrations#update", as: :update_user_registration
+    delete "user", to: "devise_/registrations#destroy", as: :destroy_user_registration
+    get "password", to: "devise/passwords#new", as: :new_user_password
+    post "password", to: "devise/passwords#create", as: :user_password
+    get "password/edit", to: "devise/passwords#edit", as: :edit_user_password
+    patch "password", to: "devise/passwords#update"
+    put "password", to: "devise/passwords#update", as: :update_user_password
+  end
 end
