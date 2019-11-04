f80535945f6c3d253e4a5f1e331d51ffeae0e603
kento <kozukata1993@gmail.com>
2019-11-04 23:48:56 +0900

    OmniAuthでログインした場合にユーザー名取得

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,7 +261,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
-  config.omniauth :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET_KEY"]
+  config.omniauth :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET_KEY"], info_fields: "email,name"
   OmniAuth.config.logger = Rails.logger if Rails.env.development?
 
   # ==> Warden configuration
